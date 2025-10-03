5382e2088ebae3552ec800780685701d4cbea99d
Ron Cantrell <ron@cantrelloffice.cloud>
2025-10-03 08:43:59 -0400

    Staging files

--- rke2nodeinit.sh
+++ rke2nodeinit.sh
@@ -18,9 +18,12 @@ esac
 
 # ----------------------------------------------------
 # rke2nodeinit.sh
+#
+#       Version: 0.6b (prelease)
 #       Written by: Ron Cantrell
 #           Github: cantrellr
 #            Email: charlescantrelljr@outlook.com
+#            
 # ----------------------------------------------------
 # Purpose:
 #   Prepare and configure a Linux VM/host (Ubuntu/Debian-based) for an offline/air-gapped
